1ccd307d94afe55db845e8ba02929e348a8a2bc4
Bruno Bennotti B Moura <brunobennotti@gmail.com>
2023-04-30 03:22:45 -0300

    feat: informações sobre previsões

--- src/TemperaturaApi/Controllers/WeatherForecastController.cs
+++ src/TemperaturaApi/Controllers/WeatherForecastController.cs
@@ -29,5 +29,17 @@ namespace TemperaturaApi.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet(Name = "informacoes/previsoes/{latitude}/{longitude}")]
+        public IEnumerable<WeatherForecast> ObterPrevisoes([FromRoute]long latitude, [FromRoute] long longitude)
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
